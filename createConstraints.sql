a14c60edfde92d4a4132ab22f4d04b7d207d28cd
aroquev00 <aroquev00@users.noreply.github.com>
2020-10-14 14:29:10 -0500

    Horse inserted in entrena must be a horse in DB

--- createConstraints.sql
+++ createConstraints.sql
@@ -3,7 +3,7 @@ returns trigger as
     $func$
     begin
         --if (not (5 = 4)) then
-        if (not (new.entrena <@ (select array_agg(registro) from caballo))) then
+        if (((select array_agg(registro) from caballo) is null) or (not (new.entrena <@ (select array_agg(registro) from caballo)))) then
             return null;
         end if;
         return new;
--- inserts.sql
+++ inserts.sql
@@ -1,5 +1,6 @@
-delete from entrenador;
+
+
 
 insert into caballo values ('caballo1', 'Pharaoh', null, 'H', 'Purebred');
 
-insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'H', 'mi casa', '818181', 1, null, '{"caballo1"}');
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'H', 'mi casa', '818181', 1, null, '{"caballo2"}');
