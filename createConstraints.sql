6d864971137eaf182332cb73b32bf93f3c493176
aroquev00 <aroquev00@users.noreply.github.com>
2020-10-14 00:03:26 -0500

    Work on triggers

---
+++ createConstraints.sql
@@ -0,0 +1,23 @@
+create or replace function tgr_checkEntrena()
+returns trigger as
+    $func$
+    begin
+        if (not (5 = 4)) then
+        --if (not (new.entrena <@ (select array_agg(registro) from caballo))) then
+            return null;
+        end if;
+        return new;
+    end;
+    $func$ language plpgsql;
+
+create trigger insert_entrenado
+    before insert
+    on entrenador
+    for each row execute procedure tgr_checkEntrena();
+
+    --when (!(new.entrena @> (select registro from caballo)))
+    --execute procedure stuff_inserting();
+
+
+
+
---
+++ inserts.sql
@@ -0,0 +1,3 @@
+delete from entrenador;
+
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'H', 'mi casa', '818181', 1, null, '{"caballo"}');
