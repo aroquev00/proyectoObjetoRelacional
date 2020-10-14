73878e2b1818b05e8d094e973fd8c15657408806
aroquev00 <aroquev00@users.noreply.github.com>
2020-10-13 22:10:14 -0500

    Create some tables

--- createTables.sql
+++ createTables.sql
@@ -1,4 +1,53 @@
-create type PersonaUdt as
-    (rfc varchar(10));
+create table caballo (
+    registro varchar(30),
+    nombre varchar,
+    fecha_nacimiento date,
+    genero char,
+    tipo varchar,
+    primary key (registro)
+);
 
-create table Persona of PersonaUdt;
+create table persona
+    (rfc varchar(12),
+    nombre varchar(50),
+    sexo char,
+    direccion varchar(100),
+    telefono varchar(10),
+    primary key (rfc)
+    );
+
+--create table persona of personaUdt;
+
+create table duenio
+    (ganancias numeric) inherits (persona);
+
+
+create table entrenador
+    (anios_experiencia int,
+    tecnica_entrena varchar[] -- deben ser referencias a caballo registro
+    ) inherits (persona);
+
+create table jockey (
+    estatura numeric,
+    peso numeric,
+    edad int,
+    salario numeric
+) inherits (persona);
+
+create table carrera (
+  num_carrera int,
+  bolsa_premio numeric,
+  fecha date,
+  tipo_carrera varchar,
+  primary key (num_carrera)
+);
+
+create table arranque (
+  num_carrera int,
+  posicion_inicio int,
+  posicion_final int,
+  color varchar(10),
+  primary key (num_carrera, posicion_inicio),
+  foreign key (num_carrera) references carrera(num_carrera)
+);
+
--- dropTables.sql
+++ dropTables.sql
@@ -1,2 +1,12 @@
+drop table carrera;
+
+drop table jockey;
+drop table entrenador;
+drop table duenio;
+
 drop table persona;
-drop type personaudt;
+--drop type personaudt;
+
+
+drop table caballo;
+
