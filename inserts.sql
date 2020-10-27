0365b45a8ba1bfd3a63e756542b0025f05cb695a
aroquev00 <aroquev00@users.noreply.github.com>
2020-10-26 18:11:18 -0600

    Add entrena trigger

--- inserts.sql
+++ inserts.sql
@@ -3,6 +3,6 @@
 
 insert into caballo values ('caballo1', 'Pharaoh', null, 'H', 'Quarter');
 
-insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'H', 'mi casa', '818181', 1, null, '{"salto"}');
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'M', 'mi casa', '818181', 1, null, '{"caballo2"}'); -- este avienta error
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'M', 'mi casa', '818181', 1, null, '{"caballo1"}'); -- este si pasa
 
-insert into persona values ('abc', null, null,null, null);
