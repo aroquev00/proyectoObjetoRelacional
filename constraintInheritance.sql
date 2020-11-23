031f1cdad96b439df02e090d0a4d173d794325fb
aroquev00 <aroquev00@users.noreply.github.com>
2020-11-23 14:44:08 -0600

    Add inheritance rules

---
+++ constraintInheritance.sql
@@ -0,0 +1,56 @@
+-- Check entrenador
+create or replace function tgr_checkEntrenador()
+returns trigger as
+    $func$
+    begin
+        if (new.rfc in (select rfc from persona)) then
+            RAISE exception 'El RFC del entrenador debe ser único';
+        end if;
+        return new;
+    end;
+    $func$ language plpgsql;
+
+drop trigger check_entrenador on entrenador;
+
+create trigger check_entrenador
+    before insert or update
+    on entrenador
+    for each row execute procedure tgr_checkEntrenador();
+
+-- Check duenio
+create or replace function tgr_checkDuenio()
+returns trigger as
+    $func$
+    begin
+        if (new.rfc in (select rfc from persona)) then
+            RAISE exception 'El RFC del dueño debe ser único';
+        end if;
+        return new;
+    end;
+    $func$ language plpgsql;
+
+drop trigger check_duenio on duenio;
+
+create trigger check_duenio
+    before insert or update
+    on duenio
+    for each row execute procedure tgr_checkDuenio();
+
+-- Check jockey
+create or replace function tgr_checkJockey()
+returns trigger as
+    $func$
+    begin
+        if (new.rfc in (select rfc from persona)) then
+            RAISE exception 'El RFC del jockey debe ser único';
+        end if;
+        return new;
+    end;
+    $func$ language plpgsql;
+
+drop trigger check_jockey on jockey;
+
+create trigger check_jockey
+    before insert or update
+    on jockey
+    for each row execute procedure tgr_checkJockey();
