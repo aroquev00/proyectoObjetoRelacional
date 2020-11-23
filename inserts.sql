48b447e8316db2fb6e8e7894633bfbad6cacde11
aroquev00 <aroquev00@users.noreply.github.com>
2020-11-23 13:55:45 -0600

    Add tests to try trigger

--- inserts.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-insert into caballo values ('caballo1', 'Pharaoh', null, 'H', 'Quarter');
-
-insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'M', 'mi casa', '818181', 1, null, '{"caballo2"}'); -- este avienta error
-insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, entrena) values ('abc', 'armando', 'M', 'mi casa', '818181', 1, null, '{"caballo1"}'); -- este si pasa
-
---
+++ insertsConstraintEntrena.sql
@@ -0,0 +1,8 @@
+
+
+
+insert into caballo values ('caballo1', 'Pharaoh', null, 'H', 'Quarter');
+
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, salario, entrena) values ('arv', 'armando', 'M', 'mty', '818181', 1, null, 1000, '{"caballo2"}'); -- este avienta error
+insert into entrenador(rfc, nombre, sexo, direccion, telefono, anios_experiencia, tecnica_entrena, salario, entrena) values ('mbc', 'marco', 'M', 'vil', '818182', 2, null, 2000, '{"caballo1"}'); -- este si pasa
+
