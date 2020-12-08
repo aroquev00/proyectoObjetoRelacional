29bf757208280dbf6843f982b26989b263106ba0
aroquev00 <aroquev00@users.noreply.github.com>
2020-12-08 14:05:49 -0600

    Final tests in demo

---
+++ test1.sql
@@ -0,0 +1,15 @@
+select * from jockey;
+
+select * from persona;
+
+INSERT INTO arranque (en_carrera,posicion_inicio,posicion_final,color,un_caballo,un_jinete) VALUES
+ (26,5,1,'rojo','ZMQMZ18279OGGRY','RRD171188KXP');--
+
+
+select * from jockey where rfc = 'RRD171188KXP';
+
+select * from entrenador where rfc = 'FVV045876GHX';
+
+select * from duenio where rfc = 'WHY482297VVM';
+
+
